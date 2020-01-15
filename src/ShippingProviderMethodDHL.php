7e38442f4113fe1e5e47b343f357f96b246d5437
Dennis Nissle <info@vendidero.de>
2020-01-15 11:43:05 +0100

    explicitly check shipping method instance settings support. Return global options as fallback in case the method does not support custom settings.

--- src/ShippingProviderMethodDHL.php
+++ src/ShippingProviderMethodDHL.php
@@ -71,12 +71,29 @@ class ShippingProviderMethodDHL {
 		}
 	}
 
+	protected function supports_settings() {
+		$supports_settings = ( $this->method->supports( 'instance-settings' ) && $this->method->supports( 'instance-settings-modal' ) ) ? true : false;
+
+		/**
+		 * Filter that allows adjusting whether this method supports DHL custom settings or not.
+		 * By default only shipping methods supporting instance-settings and instance-settings-modal are supported.
+		 *
+		 * @param boolean                   $supports_settings Whether or not the method supports custom DHL settings.
+		 * @param ShippingProviderMethodDHL $method The method instance.
+		 *
+		 * @since 3.1.1
+		 * @package Vendidero/Germanized/DHL
+		 */
+		return apply_filters( 'woocommerce_gzd_dhl_shipping_provider_method_supports_settings', $supports_settings, $this );
+	}
+
 	public function get_option( $key ) {
 		$dhl_key = $this->maybe_prefix_key( $key );
 
 		if ( $this->has_option( $key ) ) {
 
-			if ( ! $this->is_placeholder() ) {
+			// Do only use method settings if the method is not a placeholder and method supports settings
+			if ( ! $this->is_placeholder() && $this->supports_settings() ) {
 				$option_value = $this->method->get_option( $dhl_key );
 			} else {
 				$option_value = Package::get_setting( $dhl_key );
