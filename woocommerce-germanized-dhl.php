c7c6319a088af97baaf3a84215b1520e63398aa7
Dennis Nissle <info@vendidero.de>
2020-08-05 14:17:28 +0200

    Version bump.
    Signed-off-by: Dennis Nissle <info@vendidero.de>

--- woocommerce-germanized-dhl.php
+++ woocommerce-germanized-dhl.php
@@ -5,7 +5,7 @@
  * Description: The Germanized DHL integration, installed as a feature plugin for development and testing purposes.
  * Author: vendidero
  * Author URI: https://vendidero.de
- * Version: 1.2.8
+ * Version: 1.2.9
  * Requires PHP: 5.6
  * License: GPLv3
  *
