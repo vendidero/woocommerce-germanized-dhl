08b137c7b64bfc9c70827dbf2bb92585dc27dea1
Dennis Nissle <info@vendidero.de>
2020-08-05 13:38:39 +0200

    Added option for global options only.
    Signed-off-by: Dennis Nissle <info@vendidero.de>

--- src/Admin/Settings.php
+++ src/Admin/Settings.php
@@ -444,19 +444,18 @@ class Settings {
 				'default'           => '0.5',
 				'css'               => 'max-width: 60px;',
 				'class'             => 'wc_input_decimal',
-			),
-
-			array(
-				'title' 	        => _x( 'Force email', 'dhl', 'woocommerce-germanized-dhl' ),
-				'desc' 		        => _x( 'Force transferring customer email to DHL.', 'dhl', 'woocommerce-germanized-dhl' ) . '<div class="wc-gzd-additional-desc">' . _x( 'By default the customer email address is only transferred in case explicit consent has been given via a checkbox during checkout. You may force to transfer the customer email address during label creation to make sure your customers receive email notifications by DHL. Make sure to check your privacy policy and seek advice by a lawyer in case of doubt.', 'dhl', 'woocommerce-germanized-dhl' ) . '</div>',
-				'id' 		        => 'woocommerce_gzd_dhl_label_force_email_transfer',
-				'default'	        => 'no',
-				'type' 		        => 'gzd_toggle',
-			),
+			)
 		);
 
 		if ( ! $for_shipping_method ) {
 			$settings = array_merge( $settings, array(
+				array(
+					'title' 	        => _x( 'Force email', 'dhl', 'woocommerce-germanized-dhl' ),
+					'desc' 		        => _x( 'Force transferring customer email to DHL.', 'dhl', 'woocommerce-germanized-dhl' ) . '<div class="wc-gzd-additional-desc">' . _x( 'By default the customer email address is only transferred in case explicit consent has been given via a checkbox during checkout. You may force to transfer the customer email address during label creation to make sure your customers receive email notifications by DHL. Make sure to check your privacy policy and seek advice by a lawyer in case of doubt.', 'dhl', 'woocommerce-germanized-dhl' ) . '</div>',
+					'id' 		        => 'woocommerce_gzd_dhl_label_force_email_transfer',
+					'default'	        => 'no',
+					'type' 		        => 'gzd_toggle',
+				),
 				array(
 					'title' 	        => _x( 'Street number', 'dhl', 'woocommerce-germanized-dhl' ),
 					'desc' 		        => _x( 'Force existence of a street number within the first address field during checkout for EU countries.', 'dhl', 'woocommerce-germanized-dhl' ),
