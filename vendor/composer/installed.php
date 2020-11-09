b2eb8645a71835b047afffcf048a458d1a22ca9e
Dennis Nissle <info@vendidero.de>
2020-11-09 14:31:22 +0100

    Adding /vendor directory to release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7c46c407b1053aab80cdbd215bef3d865a1f08c0',
+    'name' => 'vendidero/woocommerce-germanized-dhl',
+  ),
+  'versions' => 
+  array (
+    'automattic/jetpack-autoloader' => 
+    array (
+      'pretty_version' => 'v2.5.0',
+      'version' => '2.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7945e862473e39f05e6f698f1598ea30a12e5965',
+    ),
+    'vendidero/woocommerce-germanized-dhl' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7c46c407b1053aab80cdbd215bef3d865a1f08c0',
+    ),
+  ),
+);
