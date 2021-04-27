d46a0e7f518d808b458445cd3364ccc8dda4b63d
Dennis Nissle <info@vendidero.de>
2021-04-27 13:57:54 +0200

    Adding /vendor directory to release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,64 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'cff2cfa29a2dc7fdf94171bb9e50295c5dce8d26',
+    'name' => 'vendidero/woocommerce-germanized-dhl',
+  ),
+  'versions' => 
+  array (
+    'automattic/jetpack-autoloader' => 
+    array (
+      'pretty_version' => 'v2.7.1',
+      'version' => '2.7.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5437697a56aefbdf707849b9833e1b36093d7a73',
+    ),
+    'baltpeter/internetmarke-php' => 
+    array (
+      'pretty_version' => 'v0.5.0',
+      'version' => '0.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7657ee5a55eb80c77a35e35ce71c465364d73ab4',
+    ),
+    'myclabs/deep-copy' => 
+    array (
+      'pretty_version' => '1.10.2',
+      'version' => '1.10.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '776f831124e9c62e1a2c601ecc52e776d8bb7220',
+      'replaced' => 
+      array (
+        0 => '1.10.2',
+      ),
+    ),
+    'vendidero/woocommerce-germanized-dhl' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cff2cfa29a2dc7fdf94171bb9e50295c5dce8d26',
+    ),
+    'wsdltophp/wssecurity' => 
+    array (
+      'pretty_version' => '1.2.2',
+      'version' => '1.2.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6a450af3cd462cbf73fdb3a09e80322da893af4f',
+    ),
+  ),
+);
