ab67379f86516676a306ac3f7800a16f724412b3
vendidero <info@vendidero.de>
2023-08-25 16:39:08 +0200

    Remove DHL API label notification from SOAP call as the option will be removed starting oct 2023.

--- src/Api/LabelSoap.php
+++ src/Api/LabelSoap.php
@@ -496,7 +496,6 @@ class LabelSoap extends Soap {
 							'heightInCM' => $label->has_dimensions() ? $label->get_height() : '',
 						),
 						'Service'           => $services,
-						'Notification'      => ( apply_filters( 'woocommerce_gzd_dhl_label_api_enable_notification', $label->has_email_notification(), $label ) && ! empty( $shipment->get_email() ) ) ? array( 'recipientEmailAddress' => $shipment->get_email() ) : array(),
 						'BankData'          => $bank_data,
 					),
 					'Receiver'        => array(
